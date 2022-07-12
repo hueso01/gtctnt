e92b952820a44af478c49a9ce72db83a09c90072
hueso01 <102921642+hueso01@users.noreply.github.com>
2022-07-12 16:39:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ $data = urldecode($data);
 $data = unserialize($data);
 
 if($data) {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: GET');
+    
     echo json_encode($data);
 }
 
